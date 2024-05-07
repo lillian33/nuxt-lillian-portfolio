685da872b65f09d2f00b07ce00ee17599c481024
Leon Chou <leonchou@funday.asia>
2024-05-07 18:07:01 +0800

    :wrench: config: add module & vite setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@use "@/assets/scss/variables.scss" as *;',
+				},
+			},
+		},
+	},
 	css: ["~/assets/scss/main.scss"],
+	modules: ["@unocss/nuxt"],
 });
