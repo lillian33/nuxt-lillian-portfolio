807d8a47a848d02dbf03769d6923e53a33c55f36
Leon Chou <leonchou@funday.asia>
2024-05-08 10:23:40 +0800

    🔧 config: add supabase url and key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,8 @@ export default defineNuxtConfig({
 		"/login": { prerender: true },
 	},
 	supabase: {
+		url: process.env.SUPABASE_URL,
+		key: process.env.SUPABASE_KEY,
 		redirect: true,
 		redirectOptions: {
 			login: "/login",
